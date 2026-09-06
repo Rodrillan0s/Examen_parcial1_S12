c945c65d2447568a617863aa828cdcca51ea614b
Rodrillan0s <llanosvinayarodrigo@gmail.com>
2026-09-05 22:34:39 -0400

    añadiendo despliegue productivo en el backend

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,13 @@
 import uvicorn
+import os
 from app import create_app
 
 # Instanciamos la aplicación llamando a la fábrica
-# Instanciamos la aplicación llamando a la fábrica E-Commerce Multi-Tenant V5 - Code Verification Ready
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run(
+        "run:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
